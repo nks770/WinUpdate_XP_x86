fb8790541571cc3166201748bc72d96545d57fad
nks770 <nateknute@gmail.com>
2020-09-27 11:38:25 -0600

    KB2922717 replaces KB2904266

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -4408,10 +4408,14 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 	/*if( sp==3 && (sku & XP_ALL) && _tzchange_exe<fver(5,1,2600,6188)) {
 		NN("Update for Windows XP (KB2657025)");
 		XX(p3+"WindowsXP-KB2657025-x86-ENU.exe"+a1);
-	}*/
+	}
 	if( sp==3 && (sku & XP_ALL) && _tzchange_exe<fver(5,1,2600,6479)) {
 		NN("Update for Windows XP (KB2904266)");
 		XX(p3+"WindowsXP-KB2904266-x86-ENU.exe"+a1);
+	}*/
+	if( sp==3 && (sku & XP_ALL) && _tzchange_exe<fver(5,1,2600,6502)) {
+		NN("Update for Windows XP (KB2922717)");
+		XX(p3+"WindowsXP-KB2922717-x86-ENU.exe"+a1);
 	}
 	if( sp==3 && (sku & XPE_2009) && _tzchange_exe<fver(5,1,2600,7607)) {
 		NN("Update for WES09 and POSReady 2009 (KB4468323)");
