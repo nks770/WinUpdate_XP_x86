24ae017d1f667acb650fb63af59ea28a7364c9d4
nks770 <nateknute@gmail.com>
2020-10-16 21:06:04 -0600

    Added QFE update KB947460

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -4759,6 +4759,15 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		NN("Update for Windows XP (KB944043)");
 		XX(p3+"WindowsXP-KB944043-v3-x86-ENU.exe"+a1);
 	}
+	if((sp==2 && qfe && (sku & XP_ALL) && (
+		  ( _cscdll_dll >zero && _cscdll_dll <fver(5,1,2600,3497))
+	  ||  ( _mrxsmb_sys >zero && _mrxsmb_sys <fver(5,1,2600,3497)) ))
+	 ||(sp==3 && qfe && (sku & XP_ALL) && (
+		  ( _cscdll_dll >zero && _cscdll_dll <fver(5,1,2600,5731))
+	  ||  ( _mrxsmb_sys >zero && _mrxsmb_sys <fver(5,1,2600,5731)) ))) {
+		NN("Update for Windows XP (KB947460)");
+		XX(p3+"WindowsXP-KB947460-v3-x86-ENU.exe"+a1);
+	}
 	
 
 	// Windows XP SP3 updates;
