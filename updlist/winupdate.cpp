dc09268610833025b37d8a04f938cda05b4eb6e6
nks770 <nateknute@gmail.com>
2020-10-26 23:07:00 -0600

    Added QFE update KB2892734

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -5232,6 +5232,10 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		NN("Update for Windows XP (KB2498072)");
 		XX(p3+"WindowsXP-KB2498072-x86-ENU.exe"+a1);
 	}
+	if( sp==3 && qfe && (sku & XP_ALL) && _ipnathlp_dll >zero && _ipnathlp_dll <fver(5,1,2600,6454)) {
+		NN("Update for Windows XP (KB2892734)");
+		XX(p3+"WindowsXP-KB2892734-x86-ENU.exe"+a1);
+	}
 	
 
 	// Windows XP SP3 updates;
