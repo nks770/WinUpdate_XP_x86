0036092e1365656b3b5e379cf460a6d524ac8d75
nks770 <nateknute@gmail.com>
2020-10-29 21:24:25 -0600

    Added QFE update KB950982

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -5226,6 +5226,15 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		XX(p3+"WindowsXP-KB960071-v2-x86-ENU.exe"+a1);
 	}
 	if((sp==2 && qfe && (sku & XP_ALL) && (
+		  ( _odbc32_dll   >zero && _odbc32_dll   <fver(3,525,1133,0))
+	  ||  ( _odbccp32_dll >zero && _odbccp32_dll <fver(3,525,1133,0)) ))
+	 ||(sp==3 && qfe && (sku & XP_ALL) && (
+		  ( _odbc32_dll   >zero && _odbc32_dll   <fver(3,525,3000,0))
+	  ||  ( _odbccp32_dll >zero && _odbccp32_dll <fver(3,525,3000,0)) ))) {
+		NN("Update for Windows XP (KB950982)");
+		XX(p3+"WindowsXP-KB950982-x86-ENU.exe"+a1);
+	}
+	if((sp==2 && qfe && (sku & XP_ALL) && (
 			( _sbp2port_sys >zero && _sbp2port_sys <fver(5,1,2600,3405))
 			|| _sbp2port_sys_cache <fver(5,1,2600,3405)))
 	 ||(sp==3 && qfe && (sku & XP_ALL) && (
