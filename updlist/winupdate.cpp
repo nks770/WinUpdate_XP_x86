b56faa242ed0ddf38af5113fac2577ff463e0e78
nks770 <nateknute@gmail.com>
2021-02-18 23:21:54 -0600

    Added QFE update KB885838

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -8561,6 +8561,11 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		NN("Security Update for Internet Explorer 6 Service Pack 1 (KB925486)");
 		XX(p1+"ie6.0sp1-kb925486-windowsxp-x86-enu_08de2f06f64de1e2225032d8e49e45b6f31eb8ba.exe"+a1);
 	}
+	if( sp<=1 && qfe && (sku & XP_ALL) && (_shdocvw_dll>=fver(6,0,2800,1106) && _shdocvw_dll<fver(6,0,2900,0)) && (
+		   (_iedkcs32_dll >zero && _iedkcs32_dll <fver(16,0,2800,1473)) )) {
+		NN("Update for Internet Explorer 6 Service Pack 1 (KB885838)");
+		XX(p1+"IE6.0sp1-KB885838-Windows-2000-XP-x86-ENU.exe"+a6);
+	}
 	/*if( sp==2 && (sku & XP_ALL) && (_iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)) && (
 		// GDR
 		   (_browseui_dll >zero && _browseui_dll <fver(6,0,2900,2578))
