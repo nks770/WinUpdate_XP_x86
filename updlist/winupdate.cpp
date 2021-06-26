81e66794770438755a128b61bf33fbdde1d4d628
nks770 <nateknute@gmail.com>
2021-06-26 16:38:45 -0600

    Added replaced QFE update KB894179

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -9335,6 +9335,11 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		NN("Update for Windows XP (KB892056)"); // KB892056 is replaced by KB958215
 		XX(p2+"WindowsXP-KB892056-x86-ENU.exe"+a1);
 	}*/
+	/*if( sp==2 && qfe && (sku & XP_ALL) && (_iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)) && (
+		   ( _shdocvw_dll >zero && _shdocvw_dll <fver(6,0,2900,2624)) )) {
+		NN("Update for Windows XP (KB894179)"); // KB894179 is replaced by KB958215
+		XX(p2+"WindowsXP-KB894179-x86-ENU.exe"+a1);
+	}*/
 	if( sp==2 && (sku & XP_ALL) && (_iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)) && (
 		   (_browseui_dll >zero && _browseui_dll <fver(6,0,2900,3462))
 	    || (_cdfview_dll  >zero && _cdfview_dll  <fver(6,0,2900,3462))
