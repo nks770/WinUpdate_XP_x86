5edb41f97f2433b873c7d2137aa46adc6562e7e1
nks770 <nateknute@gmail.com>
2021-07-13 22:57:45 -0600

    Added replaced QFE update KB899344

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -3287,10 +3287,10 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		NN("Update for Windows XP (KB890552)"); // KB890552 is replaced by KB970238
 		XX(p2+"WindowsXP-KB890552-x86-ENU.exe"+a6);
 	}*/
-	if( sp==2 && qfe && (sku & XP_ALL) && _rpcrt4_dll  >zero && _rpcrt4_dll  <fver(5,1,2600,2665)) {
+	/*if( sp==2 && qfe && (sku & XP_ALL) && _rpcrt4_dll  >zero && _rpcrt4_dll  <fver(5,1,2600,2665)) {
 		NN("Update for Windows XP (KB898781)"); // KB898781 is replaced by KB970238
 		XX(p2+"WindowsXP-KB898781-x86-ENU.exe"+a1);
-	}
+	}*/
 	/*if( sp==2 && (sku & XP_ALL) && _rpcrt4_dll  >zero && _rpcrt4_dll  <fver(5,1,2600,2794)) {
 		// KB908521 is replaced by KB970238 on SP2
 		NN("Update for Windows XP (KB908521)");
@@ -9595,6 +9595,11 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 			&& _mshtml_dll <fver(6,0,2900,2664)) {
 		NN("Update for Windows XP (KB898050)"); // KB898050 is replaced by KB958215
 		XX(p2+"WindowsXP-KB898050-v3-x86-ENU.exe /x:%TEMP%\\KB898050-v3 /passive");
+	}*/
+	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
+			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,2694)) {
+		NN("Update for Windows XP (KB899344)"); // KB899344 is replaced by KB958215
+		XX(p2+"WindowsXP-KB899344-x86-ENU.exe"+a1);
 	}*/
 	/*if( sp==2 && qfe && (sku & XP_ALL) && (_iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)) && (
 		   (_urlmon_dll   >zero && _urlmon_dll   <fver(6,0,2900,3313)) )) {
