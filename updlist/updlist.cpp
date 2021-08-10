f77b7f1c9b1a06807676ea540cdb788f1080b941
nks770 <nateknute@gmail.com>
2021-08-09 22:51:43 -0600

    Added replaced QFE update KB815304

--- updlist/updlist.cpp
+++ updlist/updlist.cpp
@@ -23,7 +23,7 @@
 
 using namespace std;
 
-#define PROGRAM_DATE "04-Aug-2021"
+#define PROGRAM_DATE "09-Aug-2021"
 
 int _tmain(int argc, _TCHAR* argv[]) {
 
--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -4424,6 +4424,14 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		NN("Update for Windows XP (KB889321)"); // KB889321 is replaced by KB890859
 		XX(p2+"WindowsXP-KB889321-x86-ENU.exe"+a6);
 	}*/
+	/*if((sp==1 && qfe && (sku & XP_ALL) && (
+	   ( _user32_dll   >zero && _user32_dll   <fver(5,1,2600,1619))
+	|| ( _win32k_sys   >zero && _win32k_sys   <fver(5,1,2600,1619)) ))
+	 ||(sp==2 && qfe && (sku & XP_ALL) && (
+	   ( _win32k_sys   >zero && _win32k_sys   <fver(5,1,2600,2597)) ))) {
+		NN("Update for Windows XP (KB815304)"); // KB815304 is replaced by KB890859
+		XX(p2+"WindowsXP-KB815304-x86-ENU.exe"+a6);
+	}*/
 	if((sp==1 && (sku & XP_ALL) && (
 		                  ( _authz_dll    >zero && _authz_dll    <fver(5,1,2600,1634))
 					  ||  ( _ntkrnlmp_exe >zero && _ntkrnlmp_exe <fver(5,1,2600,1634))
