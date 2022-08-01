1f0a245bf20b2d9212cbb698fbcb2d2cdf34d78d
nks770 <nateknute@gmail.com>
2022-07-31 22:20:25 -0600

    Added replaced QFE update KB914300

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -11799,6 +11799,11 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		XX(p2+"WindowsXP-KB905498-x86-ENU.exe"+a1);
 	}*/
 	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
+			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,2910)) {
+		NN("Update for Windows XP (KB914300)"); // KB914300 is replaced by KB958215(6,0,2900,3462)
+		XX(q3+"WindowsXP-KB914300-v2-x86-ENU.exe"+a1);
+	}*/
+	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
 			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,2961)) {
 		NN("Update for Windows XP (KB885439)"); // KB885439 is replaced by KB958215
 		XX(p2+"WindowsXP-KB885439-x86-ENU.exe"+a1);
