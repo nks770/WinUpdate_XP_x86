965a18a3f90e32135acfc510eb53816569a1bb65
nks770 <nateknute@gmail.com>
2022-08-02 21:35:42 -0600

    Added replaced QFE update KB915355

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -11875,6 +11875,11 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		XX(p2+"WindowsXP-KB905299-x86-ENU.exe"+a1);
 	}*/
 	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
+			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,3070)) {
+		NN("Update for Windows XP (KB915355)"); // KB915355 is replaced by KB958215
+		XX(q3+"WindowsXP-KB915355-x86-ENU.exe"+a1);
+	}*/
+	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
 			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,3213)) {
 		NN("Update for Windows XP (KB884534)"); // KB884534 is replaced by KB958215
 		XX(p2+"WindowsXP-KB884534-x86-ENU.exe"+a1);
