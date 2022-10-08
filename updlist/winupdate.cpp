ad734aa250e8be0e21e1fa92037e208d6d120a75
nks770 <nateknute@gmail.com>
2022-10-07 23:01:43 -0600

    Added replaced QFE update KB920681

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -12115,6 +12115,11 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 		XX(q3+"WindowsXP-KB918967-v2-x86-ENU.exe"+a1);
 	}*/
 	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
+			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,2936)) {
+		NN("Update for Windows XP (KB920681)"); // KB920681 is replaced by KB958215(6,0,2900,3462)
+		XX(q3+"WindowsXP-KB920681-x86-ENU.exe"+a1);
+	}*/
+	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
 			&& _mshtml_dll >zero && _mshtml_dll <fver(6,0,2900,2961)) {
 		NN("Update for Windows XP (KB885439)"); // KB885439 is replaced by KB958215
 		XX(q3+"WindowsXP-KB885439-x86-ENU.exe"+a1);
