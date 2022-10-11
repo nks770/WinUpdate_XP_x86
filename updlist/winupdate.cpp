48c5299919a7d16df6a7be4f3b3fbd5b4a195e7a
nks770 <nateknute@gmail.com>
2022-10-10 22:39:13 -0600

    Added replaced QFE update KB921400

--- updlist/winupdate.cpp
+++ updlist/winupdate.cpp
@@ -12028,7 +12028,12 @@ void windowsUpdates(std::vector<std::string>* name, std::vector<std::string>* ex
 	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
 			&& _wininet_dll >zero && _wininet_dll <fver(6,0,2900,2905)) {
 		NN("Update for Windows XP (KB908209)"); // KB908209 is replaced by KB958215(6,0,2900,3462)
-		XX(p2+"WindowsXP-KB908209-x86-ENU.exe"+a1);
+		XX(q3+"WindowsXP-KB908209-x86-ENU.exe"+a1);
+	}*/
+	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
+			&& _wininet_dll >zero && _wininet_dll <fver(6,0,2900,2961)) {
+		NN("Update for Windows XP (KB921400)"); // KB921400 is replaced by KB958215(6,0,2900,3462)
+		XX(q3+"WindowsXP-KB921400-x86-ENU.exe"+a1);
 	}*/
 	/*if( sp==2 && qfe && (sku & XP_ALL) && _iexplore_exe>=fver(6,0,0,0) && _iexplore_exe<fver(7,0,0,0)
 			&& _wininet_dll >zero && _wininet_dll <fver(6,0,2900,3073)) {
